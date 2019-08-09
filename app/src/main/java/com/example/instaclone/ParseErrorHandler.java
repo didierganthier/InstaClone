7ca3c5c9c8cbb615b0cfd439242b03e22666ab09
didierganthier <didierganthierperan@gmail.com>
2019-08-09 14:46:38 -0400

    Invalid Session Token Error handled

---
+++ app/src/main/java/com/example/instaclone/ParseErrorHandler.java
@@ -0,0 +1,21 @@
+package com.example.instaclone;
+
+import android.app.AlertDialog;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import static com.parse.ParseException.INVALID_SESSION_TOKEN;
+
+public class ParseErrorHandler {
+    public static void handleParseError(ParseException e){
+        switch (e.getCode()){
+            case INVALID_SESSION_TOKEN: handleInvalidSessionToken();
+            break;
+        }
+    }
+
+    private static void handleInvalidSessionToken(){
+        ParseUser.logOut();
+    }
+}
